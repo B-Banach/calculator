1fdc4a07405131eb8177612175fea52c40cf8a04
Bartek-B <bartlomiej.banach94@gmail.com>
2023-01-03 18:49:13 +0100

    Calculator functionality completed

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-// TODO: 
-// make calculate function!!!
-// add event listener to equals btn
-
 const allClearBtn = document.querySelector('[data-all-clear]');
 const deleteBtn = document.querySelector('[data-delete]');
 const equalsBtn = document.querySelector('[data-equals]');
@@ -22,60 +18,85 @@ allClearBtn.addEventListener('click', allClear);
 
 deleteBtn.addEventListener('click', del);
 
+equalsBtn.addEventListener('click', calculate);
+
 numberBtn.forEach(element => {
-    element.addEventListener('click', pushNumber)
-})
+    element.addEventListener('click', pushNumber);
+});
 
 operandBtn.forEach(element => {
-    element.addEventListener('click', pushOperand)
-})
+    element.addEventListener('click', pushOperand);
+});
 
+// functions
 function allClear() {
     currentOperand = '';
     currentValue = '';
     previousValue = '';
     updateDisplay();
-}
+};
 
 function del() {
     currentValue = currentValue.slice(0, -1);
     updateDisplay();
-}
+};
 
 function pushNumber(event) {
     let btnValue = event.target.innerText.toString()
     if (currentValue.includes('.') && btnValue === '.') return;
+    if (previousValue.length !== 0 && currentOperand === '') return;
     if (currentValue.length === 0 && btnValue === '.') {
         currentValue += '0';
     }
     currentValue += btnValue;
     updateDisplay();
-}
+};
 
 function pushOperand(event) {
     if (currentValue.length === 0 && previousValue.length === 0) {
         return;
+    } else if (currentValue.length !== 0 && previousValue.length === 0) {
+        previousValue = currentValue;
+        currentValue = ''
+        currentOperand = event.target.innerText.toString();
+        updateDisplay();
     } else if (currentValue.length === 0 && previousValue.length !== 0) {
         currentOperand = event.target.innerText.toString();
         updateDisplay();
-    } else {
-        // calculate(); 
+    } else if (currentValue.length !== 0 && previousValue.length !== 0){
+        calculate(); 
         currentOperand = event.target.innerText.toString();
-        previousValue = currentValue;
-        currentValue = '';
         updateDisplay();
-    }
-}
-
-
+    };
+};
 
 function updateDisplay() {
     displayCurrent.innerText = `${currentValue}`;
-    displayPrevious.innerText = `${previousValue} ${currentOperand}`
-}
-
-
+    displayPrevious.innerText = `${previousValue} ${currentOperand}`;
+};
 
 function calculate() {
+    if (currentValue.length === 0 ||
+        previousValue.length === 0 ||
+        currentOperand.length === 0) {
+        return;
+    } else {
+        if (currentOperand === '*') {
+            previousValue = parseFloat(previousValue) * parseFloat(currentValue);
+        };
+        if (currentOperand === '/') {
+            previousValue = parseFloat(previousValue) / parseFloat(currentValue);
+        };
+        if (currentOperand === '+') {
+            previousValue = parseFloat(previousValue) + parseFloat(currentValue);
+        };
+        if (currentOperand === '-') {
+            previousValue = parseFloat(previousValue) - parseFloat(currentValue);
+        };
 
-}
+        previousValue.toString();
+        currentValue = '';
+        currentOperand = '';
+        updateDisplay();
+    };
+};
